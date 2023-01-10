d8a1a78376689e76068d74a5a7d380cb7aa01bfe
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-10 16:20:30 +0530

    Refactor : EmployeeWage program for Caluculate Wage for a month

--- src/com/bridgelabzs/empwage/EmployeeWage.java
+++ src/com/bridgelabzs/empwage/EmployeeWage.java
@@ -6,17 +6,18 @@ public class EmployeeWage {
     public static void main(String[] args) {
         int Full_Day_Hours = 8;
         int Emp_RATE_PER_HOUR = 20;
+        int Total_Working_Days = 20;
         Random attendance = new Random();
         // variables
          int Check_Attendance = attendance.nextInt(3);
         switch (Check_Attendance) {
         case 1:
         	System.out.println("Employee Is Present");
-        	System.out.println("Daily employee wage is:"+(Emp_RATE_PER_HOUR*Full_Day_Hours));
+        	System.out.println("Daily employee wage is:"+(Total_Working_Days*Emp_RATE_PER_HOUR*Full_Day_Hours));
             break;
         case 2 :
         	System.out.println("Employee Is Present But PartTime:");
-        	System.out.println("Part Time Employee daily wage is:"+((Emp_RATE_PER_HOUR/2)*Full_Day_Hours));
+        	System.out.println("Part Time Employee daily wage is:"+((Emp_RATE_PER_HOUR/2)*Full_Day_Hours*Total_Working_Days));
             break;
         default:
         	System.out.println("Employee is Abscent");
