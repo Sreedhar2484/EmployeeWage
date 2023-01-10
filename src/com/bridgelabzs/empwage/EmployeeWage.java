f9133a2af8be7375280dea8139044401c1e3f4bc
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-01-10 16:08:38 +0530

    Refactor : program to caluculate PartTime Employee Wage

--- src/com/bridgelabzs/empwage/EmployeeWage.java
+++ src/com/bridgelabzs/empwage/EmployeeWage.java
@@ -1,22 +1,23 @@
 package com.bridgelabzs.empwage;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        int Full_Day_Hours = 8;
         int Emp_RATE_PER_HOUR = 20;
+        Random attendance = new Random();
         // variables
-        int empHrs = 0;
-        int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        System.out.println(empCheck);
+         int Check_Attendance = attendance.nextInt(3);
+        if(Check_Attendance == 1) {
+        	System.out.println("Employee Is Present");
+        	System.out.println("Daily employee wage is:"+(Emp_RATE_PER_HOUR*Full_Day_Hours));
+        }else if (Check_Attendance == 2){
+        	System.out.println("Employee Is Present But PartTime:");
+        	System.out.println("Part Time Employee daily wage is:"+((Emp_RATE_PER_HOUR/2)*Full_Day_Hours));
 
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else
-            empHrs = 0;
-        empWage = empHrs * Emp_RATE_PER_HOUR;
-        System.out.println("Emp wage: " + empWage);
-
-    }
-
+        }else {
+        	System.out.println("Employee is Abscent");
+        }
+	}
 }
